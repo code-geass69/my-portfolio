0e3f4176723e17609446b8e09b12e5902d1c9c8e
Atharv More <atharvmore30@gmail.com>
2025-08-03 12:55:41 +0530

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio - Atharv More",
+  title: "Atharv More",
   description: "Portfolio of Atharv More",
 };
 
